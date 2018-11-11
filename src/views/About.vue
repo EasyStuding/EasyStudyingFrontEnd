8e2f6f815bd26a923ecaed509d1b77cb41f76772
tresdosdos <vitaliystrelok8@gmail.com>
2018-11-11 16:36:48 +0300

    renew package json

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{now()}}
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'about',
+  methods: {
+    now() {
+      return new Date();
+    },
+  },
+};
+</script>
